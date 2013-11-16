1beb586158d397790aa85259276f1bfe03bcfac0
ArnaudParant <ard.parant@gmail.com>
2013-11-16 13:22:10 -0600

    added logical query operators

--- src/mongoQueryOp.ml
+++ src/mongoQueryOp.ml
@@ -1,4 +1,18 @@
 
+(*** Logical Query operator  *)
+
+let or_op d_list =
+  Bson.add_element "$or" (Bson.create_doc_element_list d_list) Bson.empty
+
+let and_op d_list =
+  Bson.add_element "$and" (Bson.create_doc_element_list d_list) Bson.empty
+
+let not_op e =
+  Bson.add_element "$not" e Bson.empty
+
+let nor_op d_list =
+  Bson.add_element "$nor" (Bson.create_doc_element_list d_list) Bson.empty
+
 (*** Query Operator Array *)
 
 let all e_list =
